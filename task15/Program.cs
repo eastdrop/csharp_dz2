e4fdd2054a44bb78407ce008289b99a7665bff6a
eastdrop <lyokha7777@gmail.com>
2023-02-14 11:50:46 +0300

    made task15

---
+++ task15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Input number: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day <= 5) Console.WriteLine("нет");
+else if (day == 6 || day == 7) Console.WriteLine("да");
+else Console.WriteLine("Такого дня не существует");
