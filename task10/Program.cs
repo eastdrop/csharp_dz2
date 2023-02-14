173a0fb7c95065bf63f319c9110e78f92e9da4f4
eastdrop <lyokha7777@gmail.com>
2023-02-14 11:12:36 +0300

    made task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,3 +1,8 @@
 ﻿Console.WriteLine("Input number: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
+if (N > 99 && N < 1000)
+{
+    Console.WriteLine(N % 100 / 10);
+}
+else Console.WriteLine("Это не трехзначное число");
